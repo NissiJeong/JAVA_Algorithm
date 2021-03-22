931f65a6fe439b3a9ea058292d2e4af0c53b7769
NissiJeong <jnissi92@gmail.com>
2021-03-22 17:23:30 +0900

    Initial commit

--- Algorithm/src/example/test/greedy/Conferenceroom.java
+++ Algorithm/src/example/test/greedy/Conferenceroom.java
@@ -1,39 +1,19 @@
 package example.test.greedy;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Conferenceroom {
+    public static int count = 0;
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int confCount = sc.nextInt();
-        List<Integer> sTimes = new ArrayList();
-        List<Integer> eTimes = new ArrayList();
+        int[] sTimes = new int[confCount];
+        int[] eTimes = new int[confCount];
+        boolean[] checks = new boolean[confCount];
         for(int i=0; i<confCount; i++){
-            sTimes.add(sc.nextInt());
-            eTimes.add(sc.nextInt());
+            sTimes[i] = sc.nextInt();
+            eTimes[i] = sc.nextInt();
         }
-
-        int minEndTime = Integer.MAX_VALUE;
-        int minIndex = -1;
-        for(int i=0; i<eTimes.size(); i++){
-            if(minEndTime>eTimes.get(i)){
-                minEndTime = eTimes.get(i);
-                minIndex = i;
-            }
-        }
-
-        int count = 0;
-        for(int i=0; i<sTimes.size(); i++){
-            if(i==minIndex || sTimes.get(i)>=minEndTime){
-                count++;
-                minEndTime = eTimes.get(i);
-                System.out.print(sTimes.get(i)+" "+eTimes.get(i));
-                System.out.println();
-            }
-        }
-        System.out.println(count);
     }
 }
