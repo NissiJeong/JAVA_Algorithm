48e0ae00e8747a58482a0f0433064050e73f2c54
NissyJeong <jnissi92@gmail.com>
2024-11-13 12:12:06 +0900

    baekjoon 2475

--- Algorithm/src/baekjoon/bronze/p2475.java
+++ Algorithm/src/baekjoon/bronze/p2475.java
@@ -1,4 +1,17 @@
 package baekjoon.bronze;
 
+import java.util.Scanner;
+
 public class p2475 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] nums = sc.nextLine().split(" ");
+
+        long validateNum = 0;
+        for (String num : nums) {
+            validateNum += Long.parseLong(num) * Long.parseLong(num);
+        }
+
+        System.out.println(validateNum % 10L);
+    }
 }
