3cd78483150f66a7a52c297fb478ec7515bd1548
NissyJeong <jnissi92@gmail.com>
2024-11-14 09:38:58 +0900

    baekjoon 10870

--- Algorithm/src/baekjoon/bronze/p10870.java
+++ Algorithm/src/baekjoon/bronze/p10870.java
@@ -3,21 +3,20 @@ package baekjoon.bronze;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-public class p10872 {
+public class p10870 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
 
-        int factorialNum = factorial(n);
+        int fiboNum = fibonacci(n);
 
-        System.out.println(factorialNum);
+        System.out.println(fiboNum);
     }
 
-    private static int factorial(int n) {
-        if(n <= 1) return 1;
-        else return n * factorial(n-1);
+    private static int fibonacci(int n) {
+        if(n == 0) return n;
+        else if(n==1) return 1 + fibonacci(n-1);
+        else return fibonacci(n-1) + fibonacci(n-2);
     }
 }
