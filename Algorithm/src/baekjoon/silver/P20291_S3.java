2cb14ba14b94c679ceaf6e111ff4f80497d52bba
NissyJeong <jnissi92@gmail.com>
2024-11-15 12:47:10 +0900

    baekjoon silber3 20291

--- Algorithm/src/baekjoon/silver/P20291_S3.java
+++ Algorithm/src/baekjoon/silver/P20291_S3.java
@@ -12,6 +12,10 @@ import java.util.*;
  * - 확장자 배열을 오름차순 정리
  * - 두개의 List 를 만들어서 String 배열 반복문을 돌면서
  * - 1개의 list 에는 단어를 다른 1개에는 count 를 저장
+ *
+ * - 실제로 문제를 풀며 최초 생각했던 풀이법과 다르게 흘러갔다.
+ * - 우선 배열로 진행하게 되면 N의 숫자가 50,000 까지 가게 되기 때문에 속도가 나지 않을 것 같았다.
+ * - 데이터를 입력 받음과 동시에 map 에 담아 중복을 줄여주고 count 를 실행하며 실행 시간을 단축하였다.
  */
 public class P20291_S3 {
     public static void main(String[] args) throws IOException {
