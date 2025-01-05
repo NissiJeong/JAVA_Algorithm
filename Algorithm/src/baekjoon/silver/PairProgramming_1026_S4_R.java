f8040d62004d8a5ad3973ab9516b2bdd7c421932
NissyJeong <jnissi92@gmail.com>
2025-01-05 10:00:00 +0900

    Git commit on January 5, 2025

---
+++ Algorithm/src/baekjoon/silver/PairProgramming_1026_S4_R.java
@@ -0,0 +1,39 @@
+package baekjoon.silver;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+/**
+ * 입력:
+ *  1.
+ * 로직:
+ *  1. 배열 a 오름차순
+ *  2. 배열 b 내림차순
+ *  3. 반복하면 값 1개씩 곱하기
+ */
+public class PairProgramming_1026_S4_R {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> a = new PriorityQueue<>();
+        PriorityQueue<Integer> b = new PriorityQueue<>(Collections.reverseOrder());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) a.add(Integer.parseInt(st.nextToken()));
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) b.add(Integer.parseInt(st.nextToken()));
+
+        int sum = 0;
+        for(int i=0; i<n; i++) {
+            sum += a.poll() * b.poll();
+        }
+
+        bw.write(sum+"");
+        bw.flush();
+    }
+}
