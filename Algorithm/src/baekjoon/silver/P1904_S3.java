4f53c434fb9ebde144383cd352ff8fccb633ba86
NissyJeong <jnissi92@gmail.com>
2025-02-05 14:49:05 +0900

    boj s1 1904

--- Algorithm/src/baekjoon/silver/P1904_S3.java
+++ Algorithm/src/baekjoon/silver/P1904_S3.java
@@ -1,4 +1,21 @@
 package baekjoon.silver;
 
+import java.util.*;
+
 public class P1904_S3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        System.out.println(fib(N));
+    }
+
+    static int fib(int n) {
+        if(n==1) return 1;
+        if(n==2) return 2;
+        int[] dp=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++) dp[i]=(dp[i-1] + dp[i-2])%15746;
+        return dp[n];
+    }
 }
--- Algorithm/src/baekjoon/silver/P1904_S3_another.java
+++ Algorithm/src/baekjoon/silver/P1904_S3_another.java
@@ -1,4 +1,25 @@
 package baekjoon.silver;
 
-public class P1904_S3_anoter {
+import java.util.*;
+
+public class P1904_S3_another {
+    static int[] dp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        dp=new int[N+1];
+        Arrays.fill(dp, -1);
+
+        System.out.println(fib(N));
+    }
+
+    static int fib(int n) {
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n]=(fib(n-1) + fib(n-2))%15746;
+    }
 }
